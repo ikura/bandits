d67b05536ca2927d9736fa118e03b2b9ad647842
Stephen Price <steve@ikuramedia.com>
2014-04-22 12:42:04 +0800

    Initial Commit

---
+++ MultiArmedBandit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MultiArmedBandit
+//
+//  Created by Price Stephen on 22/04/2014.
+//  Copyright (c) 2014 Ikura Group Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
